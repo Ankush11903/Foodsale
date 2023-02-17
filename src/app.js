dc65033e9c1017bf63a59b2f18f7171a86762d17
ankush <ankushkumar11903>
2023-02-17 22:25:01 +0530

    added app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React App</h1>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
