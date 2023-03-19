c2d498d33924bf64f855df465c2dc5386cb51c9e
ankush <ankushkumar11903@gmail.com>
2023-03-20 02:41:00 +0530

    updated shimmer

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,35 +1,32 @@
 import restaurantList from "../config";
 import RestaurantCard from "./RestaurantCard";
-import { useState,useEffect } from "react";
-import restraurantList from "../config";
+import { useState, useEffect } from "react";
 import Shimmer from "./shimmer";
 
 function fliterData(restaurant, searchText) {
-  console.log(restaurant)
+  console.log(restaurant);
   const filterData = restaurant.filter((restaurants) =>
-  restaurants?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
+    restaurants?.data?.name?.toLowerCase().includes(searchText.toLowerCase())
   );
   return filterData;
 }
 
-
-
-
 const Body = () => {
   const [searchText, setSearchText] = useState("");
   const [restaurant, setRestaurant] = useState("");
-  const [allRestaurant, setAllRestaurant] = useState("")
+  const [allRestaurant, setAllRestaurant] = useState("");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     callApiHandler();
-  }, [])
+  }, []);
 
-
-  async function callApiHandler (){
+  async function callApiHandler() {
     setLoading(true);
-    let response=await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING');
-    let da=await response.json();
+    let response = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    let da = await response.json();
     // console.log(da);
     setRestaurant(da?.data?.cards[2].data.data.cards);
     console.log(restaurant);
@@ -38,9 +35,6 @@ const Body = () => {
     setLoading(false);
   }
   
-
-  
-
   return (
     <>
       <div className="search-container">
@@ -51,29 +45,29 @@ const Body = () => {
           value={searchText}
           onChange={(e) => {
             setSearchText(e.target.value);
+            
           }}
         />
         <button
           className="search-btn"
           onClick={() => {
             let data = fliterData(allRestaurant, searchText);
-            setRestaurant( data);
+            setRestaurant(data);
           }}
         >
           Search
         </button>
       </div>
       <div className="flex flex-wrap">
-        {restaurant?.length === 0 ? <Shimmer /> :
-        restaurant.map((restaurant) => {
-          return (
-            
-            <RestaurantCard
-              {...restaurant.data}
-              key={restaurant.data.id}
-            />
-          );
-        })}
+        {restaurant?.length === 0 ? (
+          <Shimmer />
+        ) : (
+          restaurant.map((restaurant) => {
+            return (
+              <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+            );
+          })
+        )}
       </div>
     </>
   );
--- src/components/shimmer.js
+++ src/components/shimmer.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function Shimmer() {
+const Shimmer = () => {
   return (
-    <h1>Rendering....</h1>
-  )
-}
+        <div className="flex flex-wrap">
+          {Array(10)
+            .fill("")
+            .map((e, index) => (
+              <div key={index} className="w-[254px] h-[220px] p-3 m-3 border border-black bg-gray-100">
+                
+                <div className="w-full h-[135px] border border-black bg-zinc-50"></div>
+                
+                </div>
+            ))
+            
+            }
+        </div>
+      );
+     };
+     
+     export default Shimmer;
