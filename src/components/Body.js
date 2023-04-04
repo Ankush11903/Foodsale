4b4b55bb7914c5d173d30aa6fc6b52d4a1ea9251
ankush <ankushkumar11903@gmail.com>
2023-04-04 21:10:48 +0530

    fixed carousal slider

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 import FoodCarousel from "./foodCarousel";
 
 function fliterData(restaurant, searchText) {
-  // console.log(restaurant);
   const filterData = restaurant.filter((restaurants) =>
     restaurants?.name?.toLowerCase().includes(searchText.toLowerCase())
   );
@@ -27,84 +26,53 @@ const Body = () => {
   }, []);
 
   async function callApiHandler() {
-    // setLoading(true);
     let response = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING"
     );
     let da = await response.json();
     console.log(da);
-    // setRestaurant(da?.data?.cards[2]?.data?.data?.cards);
-    // console.log(restaurant);
+
     rest = da?.data?.cards[2]?.data?.data?.cards?.map((x) => x.data);
     setNoOfRestaurant(da?.data?.cards[2]?.data?.data?.totalRestaurants);
-    // console.log(allRestaurant);
 
-    // console.log(rest)
     setRestaurant(rest);
     setAllRestaurant(rest);
     setCarouselData(da?.data?.cards[0]?.data?.data?.cards);
-    // console.log(allRestaurant)
-
-    // setLoading(false);
   }
 
   async function callApiHandler2() {
-    // setLoading(true);
     let response2 = await fetch(
       `https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&offset=${cart}&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING`
     );
     let dat = await response2.json();
-    // console.log("dat",dat)
-    // setRestaurant(...restaurant,dat?.data?.cards);
+
     rest?.push(
       ...dat?.data?.cards
         ?.map((x) => x)
         ?.filter((x) => x.cardType == "restaurant")
         ?.flat()
-        .map(
-          (x) =>
-            // console.log(x.data.data)
-            x.data?.data
-        )
+        .map((x) => x.data?.data)
     );
 
-    // console.log(rest)
     setRestaurant((prev) => [...prev, ...rest]);
     setAllRestaurant((prev) => [...prev, ...rest]);
-    // loading=false;
-    // console.log(allRestaurant)
-
-    // setLoading(false);
   }
 
   useEffect(() => {
-    // console.log("calling")
     callApiHandler2();
-    // setLoading(false);
-    // loading=false;
 
-    return () => {
-      // console.log("unmounting")
-      // loading=false;
-      // setLoading(false);
-    };
+    return () => {};
   }, [cart]);
 
   const handelInfiniteScroll = async () => {
-    // console.log("scrollHeight" + document.documentElement.scrollHeight);
-    // console.log("innerHeight" + window.innerHeight);
-    // console.log("scrollTop" + document.documentElement.scrollTop);
     try {
       if (
         window.innerHeight + document.documentElement.scrollTop + 1 >=
         document.documentElement.scrollHeight
       ) {
-        // setLoading(true);
         setCart((prev) => prev + 16);
       }
-    } catch (error) {
-      // console.log(error);
-    }
+    } catch (error) {}
   };
 
   useEffect(() => {
@@ -114,38 +82,42 @@ const Body = () => {
 
   return (
     <>
-      {/* <div className="search-container mx-72 py-6 flex">
-        <input
-          type="text"
-          className="search-input border border-[#e4e3e3] rounded-md"
-          placeholder="Search"
-          value={searchText}
-          onChange={(e) => {
-            setSearchText(e.target.value);
-          }}
-        />
-        <button
-          className="search-btn mx-3"
-          onClick={() => {
-            let data = fliterData(allRestaurant, searchText);
-            setRestaurant(data);
-          }}
-        >
-          Search
-        </button>
-      </div> */}
-      <div className=" bg-[#171a29]" ><FoodCarousel data={carouselData}/></div>
-      <div className="font-semibold text-[#232737] text-3xl pt-10 pl-8 pb-2">{noOfRestaurant} restaurants</div>
+      <div className=" bg-[#171a29]">
+        <FoodCarousel data={carouselData} />
+      </div>
+      <div className="flex justify-between">
+        <div className="font-semibold text-[#232737] text-2xl pt-10 pl-8 pb-2">
+          {noOfRestaurant} restaurants
+        </div>
+        <div className="search-container mx-72 py-6 flex">
+          <input
+            type="text"
+            className="search-input border border-[#e4e3e3] rounded-md"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn mx-3 font-normal"
+            onClick={() => {
+              let data = fliterData(allRestaurant, searchText);
+              setRestaurant(data);
+            }}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
       <hr className="border-[#e4e3e3] mx-8 " />
       <div className="flex flex-wrap ">
         {restaurant?.length === 0 ? (
           <Shimmer />
         ) : (
-          // <>
-          
           restaurant?.map((restaurant) => {
             return (
-              // console.log(restaurant)
               <RestaurantCard {...restaurant} key={restaurant?.data?.id} />
             );
           })
@@ -158,7 +130,6 @@ const Body = () => {
               className=" animate-pulse w-[254px] h-[220px] p-3 m-5 border border-slate-200 rounded-md bg-[#fdfdfd]"
             >
               <div className="animate-pulse w-full h-[135px] border border-neutral-300 rounded-md bg-gray-100"></div>
-              {/* <br /> */}
 
               <p class="leading-relaxed mt-4 mb-2 w-full h-3 animate-pulse bg-gray-200 rounded-sm"></p>
               <p class="leading-relaxed mt-2 mb-1 w-2/3 h-3 animate-pulse bg-gray-200 rounded-sm"></p>
