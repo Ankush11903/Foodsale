e5f1bce258a0429deee15d91a8a2e37580729519
Ankush kumar <ankushkumar11903@gmail.com>
2023-05-23 23:47:09 +0530

    docs-footer
    Co-Authored-By: Aniket Goyal <129101494+aniket110903@users.noreply.github.com>

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ const About=()=>{
     return (
         <div>
             <h1>About</h1>
-            <p>This is a simple react app to demonstrate the use of react-router-dom</p>
+            <p className="font-serif">This App is created by ankush</p>
 
         </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logo from "../logo.jpg";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="bg-gray-200 py-5">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* Additional content or components */}
+        </div>
+        <div className="text-center text-gray-600 mt-2">
+          &copy; {new Date().getFullYear()} Foosale. All rights reserved.
+          <span className="text-sm text-gray-400">Website created by Ankush</span>
+        </div>
+      </div>
+    </footer>
+  );
 }
