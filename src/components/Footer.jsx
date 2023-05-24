14c9107ccf787b5e9230e2267a5b899707382f44
Ankush kumar <ankushkumar11903@gmail.com>
2023-05-24 11:28:44 +0530

    Revert "docs-footer"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,7 @@
-import React from 'react';
-import logo from "../logo.jpg";
+import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 py-5">
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          {/* Additional content or components */}
-        </div>
-        <div className="text-center text-gray-600 mt-2">
-          &copy; {new Date().getFullYear()} Foosale. All rights reserved.
-          <span className="text-sm text-gray-400">Website created by Ankush</span>
-        </div>
-      </div>
-    </footer>
-  );
+    <div>Footer</div>
+  )
 }
